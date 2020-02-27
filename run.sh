c563e89b1b2ff0a4ffa5be45d3864f529024bee6
orris27 <gsorris27@gmail.com>
2020-02-27 17:25:05 +0800

    inti

---
+++ run.sh
@@ -0,0 +1,10 @@
+python3 downloader.py \
+    -data_root /home/orris/datasets/imagenet \
+    -number_of_classes 50 \
+    -images_per_class 500
+
+#python ./downloader.py \
+#    -data_root /home/orris/datasets/imagenet \
+#    -use_class_list True \
+#    -class_list n09858165 n01539573 n03405111 \
+#    -images_per_class 500 
